3af1971aa06ae6091b9d3fac2dc92a0a228e35f2
Nidhi <nidhidama25@gmail.comgit config --global user.email nidhidama25@gmail.comgit config --global user.email nidhidama25@gmail.com>
2025-01-06 13:25:15 +0530

    Comment functionality added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookiePaser = require("cookie-parser");
+
 const Blog = require("./models/blog");
 
 const { checkForAuthenticationCookie } = require("./middlewares/auth");
@@ -10,9 +11,11 @@ const PORT = 8000;
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
+
 mongoose
   .connect("mongodb://localhost:27017/blogSocial")
   .then((e) => console.log("MongoDB Connected"));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
@@ -21,6 +24,7 @@ app.use(express.json());
 app.use(cookiePaser());
 app.use(express.static(path.resolve("./public")));
 app.use(checkForAuthenticationCookie("token"));
+
 app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
   res.render("home", {
@@ -28,6 +32,8 @@ app.get("/", async (req, res) => {
     blogs: allBlogs,
   });
 });
+
 app.use("/user", userRoute);
 app.use("/blog", blogRoute);
+
 app.listen(PORT, () => console.log(`Server Started at PORT :${PORT}`));
