7d3094763e0eeb0080f6da76d8da0c576bafe3d1
Nidhi <nidhidama25@gmail.comgit config --global user.email nidhidama25@gmail.comgit config --global user.email nidhidama25@gmail.com>
2025-01-06 12:48:18 +0530

    added routes for single blog pages and used Multer for upload functionality

--- index.js
+++ index.js
@@ -2,25 +2,32 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookiePaser = require("cookie-parser");
-const userRoute = require("./routes/user");
+const Blog = require("./models/blog");
+
 const { checkForAuthenticationCookie } = require("./middlewares/auth");
 const app = express();
 const PORT = 8000;
 
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
 mongoose
   .connect("mongodb://localhost:27017/blogSocial")
   .then((e) => console.log("MongoDB Connected"));
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookiePaser());
+app.use(express.static(path.resolve("./public")));
 app.use(checkForAuthenticationCookie("token"));
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   });
 });
-
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 app.listen(PORT, () => console.log(`Server Started at PORT :${PORT}`));
